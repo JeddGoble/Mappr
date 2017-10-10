f2fb2d7645a4d5885b11db40992eb0ee567bb96c
Jedd Goble <Jedidiah.Goble@teamaol.com>
2017-10-10 14:07:55 -0700

    Initial Commit

---
+++ ARFlickr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARFlickr
+//
+//  Created by jgoble52 on 10/10/17.
+//  Copyright © 2017 Jedd Goble. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
